426a4b520a02a84a727a4d1fa47f7d9994ec86dc
James Percival <j.percival@imperial.ac.uk>
2020-06-17 07:54:40 +0100

    Initial commit.

---
+++ app.py
@@ -0,0 +1,21 @@
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    now = time.localtime()
+
+    return f"""
+<h1> Welcome! </h1>
+
+It is {now.tm_hour}:{now.tm_min:02} on {now.tm_mday}/{now.tm_mon}/{now.tm_year}.
+"""
+
+@app.route('/secret')
+def secret():
+    return 'You found me!'
+
